12323bb454db4c6596c86562a70a198485cc26ca
keefske2000 <keith.w.collins@gmail.com>
2019-04-10 21:18:33 -0400

    merge changes

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Categories from './containers/Categories';
 import './MainRouter.css';
 import Loginscreen from "./Loginscreen";
+<<<<<<< Updated upstream
 import CartContext from './CartContext';
 import Cart from './containers/Cart';
+=======
+import Navbar from "./components/Navbar";
+>>>>>>> Stashed changes
 
 
 // Routes
@@ -20,7 +24,7 @@ const routes = [
     },
     {
         path: "/login",
-        component: () => <h2>shoelaces</h2>
+        component: () => <h2></h2>
     },
     {
         path: "/cart",
@@ -30,6 +34,10 @@ const routes = [
         path: "/Login",
         component: () => <Loginscreen />
 
+    },
+    {
+        path: "/Navbar",
+        component: () => <Navbar />
     }
 ];
 
@@ -37,6 +45,7 @@ function RouterContainer() {
     const cart = useContext(CartContext);
     return (
         <Router>
+<<<<<<< Updated upstream
             <div className="container">
 
                 <div className="header">
@@ -48,6 +57,12 @@ function RouterContainer() {
                 </div>
 
                 <div className="main">
+=======
+           
+            <Navbar />
+              
+              <div className="main">
+>>>>>>> Stashed changes
                     {routes.map((route, index) => (
                         
                         <Route
@@ -59,12 +74,40 @@ function RouterContainer() {
                     ))}
                 </div>
 
+<<<<<<< Updated upstream
             
 
                 <div className="footer">
                     Some Footer
                 </div>
+=======
+                <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
             </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+>>>>>>> Stashed changes
+            </div>
+          </div>
+        </footer>
+           
         </Router>
     );
 }
