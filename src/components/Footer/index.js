34c9365040abd2147b74fa4a1452fc29d2b559ad
keefske2000 <keith.w.collins@gmail.com>
2019-04-13 10:47:31 -0400

    updated keith branch

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+   <div class="footer-copyright">
+            <div class="container">
+            © 2019 Copyright Text
+            <a class="black-text text-lighten-4 right" href="#!">Contact Us Here</a>
+            </div>
+          </div>
+  );
+}
+
+export default Footer;
