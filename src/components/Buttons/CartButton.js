75bd24373162c6cf71c524cf712861860d3ddbd0
Nali M <nmavilmada@gmail.com>
2019-04-19 00:26:30 -0400

    cart count

--- src/components/Buttons/CartButton.js
+++ src/components/Buttons/CartButton.js
@@ -6,11 +6,12 @@ import CartContext from '../../CartContext';
 
 export default function CartButton(props) {
   const cart = useContext(CartContext);
+  const productCount = cart.products.filter(p => p.id === props.product.id).length;
   return (
     <button
       onClick={() => cart.addProduct(props.product)}
     >
-      Add to Cart
+      Add to Cart { productCount > 0 && `[${productCount}]`}
     </button>
   );
 }
